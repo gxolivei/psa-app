0d8dc16cf881616b34e648df13db42b14bb8b4fb
Gabriel Oliveira <gxolivei@gmail.com>
2023-09-25 15:02:36 -0300

    initial config

---
+++ app/javascript/components/App.vue
@@ -0,0 +1,18 @@
+<template lang="">
+  <div>
+    <h1>Vue</h1>
+    <Counter />
+  </div>
+</template>
+<script>
+  import Counter from './Counter.vue'
+
+  export default {
+    components: {
+      Counter
+    }
+  }
+</script>
+<style lang="">
+  
+</style>
---
+++ app/javascript/components/Counter.vue
@@ -0,0 +1,35 @@
+<template lang="">
+  <div>
+    <button @click="increment" id="counter-button">increment</button>
+    <p>Count is: {{ count }}</p>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    }
+  }
+}
+</script>
+<style lang="css">
+  #counter-button {
+    background-color: #000;
+    color: #fff;
+    border: none;
+    padding: 10px 20px;
+    border-radius: 5px;
+    border-radius: 5px;
+  }
+
+  #counter-button:hover {
+    background-color: #333;
+    cursor: pointer;
+  }
+</style>
